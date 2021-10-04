212f92d374c352739cc1bee223ae3272d416a68e
Joana <joanapiovaroli@gmail.com>
2021-10-03 23:37:06 -0300

    Delete from repository

--- src/main/java/arg/com/bioparque/data/PersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package arg.com.bioparque.data;
-
-import arg.com.bioparque.domain.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long>{
-    Persona findByUserName(String userName);
-}
--- src/main/java/arg/com/bioparque/data/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package arg.com.bioparque.data;
-
-import arg.com.bioparque.domain.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    Usuario findByUsername(String username);
-}
